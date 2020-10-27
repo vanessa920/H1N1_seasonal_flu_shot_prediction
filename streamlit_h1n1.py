0a2c692d212a6a1dfc2a2fa25578185074990a39
Vanessa Hu <3359992+vanessa920@users.noreply.github.com>
2020-10-27 00:22:38 -0700

    update proba

--- streamlit_h1n1.py
+++ streamlit_h1n1.py
@@ -14,14 +14,43 @@ Body_html = """
     color: #7a7477;
 
   }
+  h2{
+    color:#F3005E;
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%)
+  }
+  h3{
+    color:#898989;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    size: 200%;
+  }
+
+  h4{
+    color: #7a7477;
+  }
+
+ img{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    #width: 300px;
+    height: auto;
+    opacity:0.8;
+}
+
    body{
     background-image: url(https://i.stack.imgur.com/HCfU2.png);
     #https://i.stack.imgur.com/9WYxT.png
-    opacity: 0.9;
+    #opacity: 0.5;
 }
 
-
-    <h1>How Likely One Takes H1N1 Vaccine?</h1>
 </style>
 
 
@@ -33,10 +62,22 @@ st.write(
 """
 
 # How Likely One Takes H1N1 Vaccine?
+
+![vaccine_img](http://bento.cdn.pbs.org/hostedbento-prod/filer_public/spillover/images/viruses/definitions/vaccines.png)
+
+#### Try the tool to predict whether people got H1N1 vaccines using information they shared about their healthcare backgrounds and opinions.
+
+***
+
+In Spring 2009, a pandemic caused by the H1N1("Swine Flu") influenza virus, swept across the world. A vaccine for the H1N1 flu virus became publicly available in October 2009.
+In late 2009 and early 2010, the United States conducted the National 2009 H1N1 Flu Survey. More details about this dataset and features available at [DrivenData](https://www.drivendata.org/competitions/66/flu-shot-learning/page/211/).
+
+***
+
 """
 )
 
-st.sidebar.header('User Imput Parameters')
+#st.sidebar.header('User Imput Parameters')
 
 
 st.sidebar.markdown("""
@@ -77,8 +118,6 @@ else:
 
     df = user_input_feature()
 
-    st.subheader('User Input Parameters')
-    st.write(df)
 
     # df_drop = pd.read_pickle("Data/df_drop.pkl")
     # df_train=df_drop.values
@@ -98,5 +137,14 @@ else:
     #lr_model.fit(X, y)
 
     lr_model=pd.read_pickle("Data/lr_model.pickle")
-    prediction_proba=lr_model.predict_proba(df)
-    st.write(prediction_proba[0][1])
+    #ada_model=pd.read_pickle("models/ada_model.pickle")
+    prediction_proba_lr=lr_model.predict_proba(df)
+    #prediction_proba_ada=ada_model.predict_proba(df)
+    st.subheader('Based on survey, the possibility of One Receives H1N1 Vaccine is:')
+    st.header("{:.0%}".format(prediction_proba_lr[0][1]))  #print "{:.0%}".format(1/3)
+    #st.write(prediction_proba_ada[0][1])
+    # st.subheader('User Input Parameters')
+    # st.write(df)
+    st.write("""
+    ![import_features_logistic_Regression]("Important Features/slide2.png")
+    """)
