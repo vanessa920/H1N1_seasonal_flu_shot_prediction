8825a5da5bfcfc26d80e33550337f08f7df6e9b1
Vanessa Hu <3359992+vanessa920@users.noreply.github.com>
2020-10-31 23:37:04 -0700

    app py file

--- streamlit_h1n1.py
+++ streamlit_h1n1.py
@@ -35,6 +35,10 @@ Body_html = """
     color: #7a7477;
   }
 
+  h5{
+    color: lightgrey;
+  }
+
  img{
     display: block;
     margin-left: auto;
@@ -42,8 +46,9 @@ Body_html = """
     width: 50%;
     #width: 300px;
     height: auto;
-    opacity:0.8;
+    #opacity:0.9;
 }
+
 
    body{
     background-image: url(https://i.stack.imgur.com/HCfU2.png);
@@ -61,46 +66,45 @@ st.markdown(Body_html, unsafe_allow_html=True) #Body rendering
 st.write(
 """
 
-# How Likely One Takes H1N1 Vaccine?
+# Will a Person get the H1N1 Vaccine?
 
 ![vaccine_img](http://bento.cdn.pbs.org/hostedbento-prod/filer_public/spillover/images/viruses/definitions/vaccines.png)
 
-#### Try the tool to predict whether people got H1N1 vaccines using information they shared about their healthcare backgrounds and opinions.
+#### Try the tool to predict whether a person will get the H1N1 vaccine using the information they shared about their healthcare background and opinions.
 
 ***
 
-In Spring 2009, a pandemic caused by the H1N1("Swine Flu") influenza virus, swept across the world. A vaccine for the H1N1 flu virus became publicly available in October 2009.
-In late 2009 and early 2010, the United States conducted the National 2009 H1N1 Flu Survey. More details about this dataset and features available at [DrivenData](https://www.drivendata.org/competitions/66/flu-shot-learning/page/211/).
+In Spring 2009, a pandemic caused by the H1N1 influenza virus ("Swine Flu"), swept across the world. A vaccine for the H1N1 flu virus became publicly available in October 2009.
+In late 2009 and early 2010, the United States conducted the National 2009 H1N1 Flu Survey. More details about this dataset and features are available at [DrivenData.org](https://www.drivendata.org/competitions/66/flu-shot-learning/page/211/).
 
 ***
 
 """
 )
 
-#st.sidebar.header('User Imput Parameters')
-
 
 st.sidebar.markdown("""
 [Example CSV input file]("https://raw.githubusercontent.com/vanessa920/H1N1_seasonal_flu_shot_prediction/eda/Data/example_data.csv")
 """)
 
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+uploaded_file = st.sidebar.file_uploader("Upload your CSV input file", type=["csv"])
+st.sidebar.subheader('Tool Input Parameters')
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_feature():
-        doctor_recc_h1n1 = st.sidebar.checkbox('H1N1 Vaccine Recommened by Doctor?')
-        doctor_recc_seasonal = st.sidebar.checkbox('Seasonal Flu Vaccine Recommened by Doctor?')
+        doctor_recc_h1n1 = st.sidebar.checkbox('H1N1 Vaccine Recommended by Doctor?')
+        doctor_recc_seasonal = st.sidebar.checkbox('Seasonal Flu Vaccine Recommended by Doctor?')
         seasonal_vaccine = st.sidebar.checkbox('Received Seasonal Flu Vaccine?')
         health_worker = st.sidebar.checkbox('Working in Healthcare Industry?')
         h1n1_knowledge = st.sidebar.slider('Level of H1N1 Knowledge:', 0, 2, 1)
         h1n1_concern =st.sidebar.slider('Level of H1N1 Concern:', 0, 3, 2)
         opinion_h1n1_risk = st.sidebar.slider('Opinion: Is H1N1 Virus Risky?:', 0, 5, 1)
-        opinion_h1n1_sick_from_vacc = st.sidebar.slider('How Risky do you think H1N1 Vaccine?:', 0, 5, 2)
-        opinion_h1n1_vacc_effective = st.sidebar.slider('How Effective do you think H1N1 Vaccine?:', 0, 5, 3)
-        opinion_seas_risk=st.sidebar.slider('How Much do you think Seasonal Flu is Risky?:', 0, 5, 5)
-        opinion_seas_sick_from_vacc=st.sidebar.slider('How Risky do you think Seasonal Vaccine?:', 0, 5, 4)
-        opinion_seas_vacc_effective=st.sidebar.slider('How Effective do you think Seasonal Vaccine?:', 0, 5, 3)
+        opinion_h1n1_sick_from_vacc = st.sidebar.slider('Opinion: H1N1 Vaccine Makes You Sick?:', 0, 5, 2)
+        opinion_h1n1_vacc_effective = st.sidebar.slider('Opinion: Is H1N1 Vaccine Effective?:', 0, 5, 3)
+        opinion_seas_risk=st.sidebar.slider('Opinion: Is Seasonal Flu Risky?:', 0, 5, 5)
+        opinion_seas_sick_from_vacc=st.sidebar.slider('Opinion: Seasonal Vaccine Makes You Sick?:', 0, 5, 4)
+        opinion_seas_vacc_effective=st.sidebar.slider('Opinion: Is Seasonal Vaccine Effective?:', 0, 5, 3)
         data = {'doctor_recc_h1n1': doctor_recc_h1n1,
                 'doctor_recc_seasonal': doctor_recc_seasonal,
                 'seasonal_vaccine': seasonal_vaccine,
@@ -140,11 +144,67 @@ else:
     #ada_model=pd.read_pickle("models/ada_model.pickle")
     prediction_proba_lr=lr_model.predict_proba(df)
     #prediction_proba_ada=ada_model.predict_proba(df)
-    st.subheader('Based on survey, the possibility of One Receives H1N1 Vaccine is:')
+    st.subheader('Based on the survey, the probability of getting the H1N1 Vaccine is:')
     st.header("{:.0%}".format(prediction_proba_lr[0][1]))  #print "{:.0%}".format(1/3)
     #st.write(prediction_proba_ada[0][1])
     # st.subheader('User Input Parameters')
     # st.write(df)
-    st.write("""
-    ![import_features_logistic_Regression]("Important Features/slide2.png")
-    """)
+    # st.write("""
+    # ![import_features]("Important Features/slide2.png")
+    # """)
+st.write(""" *** """)
+
+st.write("""
+Feature Coefficient in Logistic Model
+""")
+# feature_coef = pd.read_csv("Data/feature_coef.csv")
+#
+# ax = plt.bar(x='Coefficient', y='Feature', data=feature_coef,
+#                  color = "#F3005E", order=feature_coef.sort_values('Coefficient', ascending = False).Feature)
+#
+# st.pyplot(feature_coef.Feature,use_container_width=True)
+
+from PIL import Image
+image_coef = Image.open('Important Features/Feature_Coeffient.jpg')
+st.image(image_coef,use_column_width=True)
+
+# image_rf = Image.open('Important Features/Features_in_rf.png')
+# st.image(image_rf, caption='Feature Importance in Logistic Model',use_column_width=True)
+st.write(""" *** """)
+st.subheader("Methodology Diagram")
+st.graphviz_chart('''
+digraph {
+    EDA -> Correlation
+    EDA -> NAs
+    EDA -> Transform
+    Transform -> OneHotEncode
+    Transform -> LabelEncode
+    NAs -> MiceImput
+    OneHotEncode -> Modeling
+    MiceImput -> Modeling
+    Modeling -> RandomForest
+    Modeling -> CatBoost
+    Modeling -> GBoost
+    Modeling -> SVC
+    Modeling -> DecisionTree
+    Modeling -> NB
+    Modeling -> KNN
+    Modeling -> Logistic
+    Logistic -> Pipeline
+    ADA -> Pipeline
+    ET -> Pipeline
+    NB -> Pipeline
+    KNN -> Pipeline
+    SVC -> Pipeline
+    RandomForest -> Pipeline
+    RandomForest -> ImportantFeatures
+    CatBoost -> ImportantFeatures
+    ImportantFeatures -> Logistic
+    Logistic -> Prediction
+    Prediction -> Visualization
+}
+''')
+
+st.markdown("""
+##### Copyright@VanessaHu
+""")
